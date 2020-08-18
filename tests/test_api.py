40d9c710293c0d602623a15dfccbf2058166fbe4
remche <remche@remche.org>
2020-08-18 12:33:26 +0200

    Revert "test url"
    This reverts commit 622d59a886ed992c6c68f6de29803234f2fcb690.

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,13 @@ import requests
 
 def test_proxyspec():
     url = os.environ.get("SHINYPROXY_URL", "http://shiny.test:8000")
-    r = requests.get("http://shiny.test:8000/api/proxyspec")
+    r = requests.get(f"{url}/api/proxyspec")
     assert r.status_code == 200
     assert len(r.json()) == 2
 
 
 def test_spawn():
     url = os.environ.get("SHINYPROXY_URL", "http://shiny.test:8000")
-    r = requests.post("http://shiny.test:8000/api/proxy/01_hello")
+    r = requests.post(f"{url}/api/proxy/01_hello")
     assert r.status_code == 201
     assert r.json().get("status") == "Up"
